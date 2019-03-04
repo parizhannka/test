0dd85812a5c24747305d3c5bccfb8fffd5248df5
user <user@mail.com>
2019-03-04 11:21:23 +0300

    Задача 1

---
+++ src/main/java/Zadacha.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Zadacha {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+      float z;
+      System.out.println("Введите первое число");
+        z=input.nextFloat();
+        float w;
+        System.out.println("Введите второе число");
+        w=input.nextFloat();
+        if (Math.abs(20 - z) == Math.abs(20 - w))
+            System.out.println("Числа z и w равноудалены от 20");
+         else if
+                (Math.abs(20 - z) < Math.abs(20 - w))
+            System.out.println("Число " + z + " ближе к числу " + 20);
+        else
+            System.out.println("Число " + w + " ближе к числу " + 20);
+            }
+}
